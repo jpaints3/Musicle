7a68d57fb1ac164e6404b5f6d5b751bc09a04369
Josiah Paintsil <josiahpaintsil@josiahs-mbp.colgate.edu>
2020-09-19 22:20:51 -0400

    playlist react app

---
+++ views/js/main.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import PlaylistApp from 'PlaylistApp.js';
+
+
+class Main extends Component{
+    render(){
+        return (
+            <div>
+                <PlaylistApp/>
+            </div>
+        );
+    }
+}
+
+render(<Main/>, document.getElementById("root"))
+
+
--- views/js/playlistApp.js
+++ views/js/playlistApp.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
-function playlistApp(){
-    return (
-        <div>
-            <p>Am I working?</p>
-        </div>
-    )
+class PlaylistApp extends Component{
+    render(){
+        return(
+            <div>
+                <p>Am I working?</p>
+            </div>
+        );
+    }
 }
 
-export default playlistApp;
+export default PlaylistApp;
