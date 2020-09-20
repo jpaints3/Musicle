66df6c2486aaea5fde49b4ee8e16ee8b582f20a9
Josiah Paintsil <josiahpaintsil@josiahs-mbp.colgate.edu>
2020-09-19 21:53:31 -0400

    playlist react app

--- views/js/playlistApp.js
+++ views/js/playlistApp.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+function playlistApp(){
+    return (
+        <div>
+            <p>Am I working?</p>
+        </div>
+    )
+}
+
+export default playlistApp;
